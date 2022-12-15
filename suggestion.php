b62bc797a7af30153cc9fbdcc6875e2d29aaa67f
albert20699 <86821598+albert20699@users.noreply.github.com>
2022-12-15 22:41:41 +0800

    Delete suggestion.php

--- suggestion.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if(!isset($_SESSION)) 
-	{ 
-		session_start(); 
-	} 
-	$_SESSION['username'] = NAN;
-	$_SESSION['password']   = NAN;
-	echo "<script>alert('以登出!!!');location.href='login.php';</script>";
-?> 
